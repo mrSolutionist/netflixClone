6ab1a6942b423e86f49af0f9533fb991d6b18408
ROBIN GEORGE <therobins.dev@gmail.com>
2021-12-02 23:13:59 +0530

    test

--- NetflixClone/Controllers/HomeVC.swift
+++ NetflixClone/Controllers/HomeVC.swift
@@ -24,7 +24,7 @@ class HomeVC: UIViewController {
         }
     }
     
-    var coreGenre : GenreList?
+    var coreGenre : [GenreList]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -63,7 +63,7 @@ class HomeVC: UIViewController {
 extension HomeVC : UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return genrelist?.genres?.count ?? 0
+        return coreGenre?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
